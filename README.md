3fd42817a82cedd9aeb311b6e2bc2c1c9c908491
realChrisDeBon <christopherdebon@gmail.com>
2025-10-11 23:25:30 -0500

    1. updating README with basic info 2. updating run.bat to have more accurate path placeholders

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # SETUP INSTRUCTIONS
 
-Here are where setup instructions will go.
+## Requirements
+
+   1. Git
+   2. SQL Server Express
+   3. Dotnet SDK Version X.X (TODO: Fill this out)
+
+## Steps
+
+   1. Run `git clone --recurse-submodules https://github.com/ITD333GroupB/Main.git`
+   2. Navigate to the 'Main' directory
+   3. Execute `run.bat`
--- run.bat
+++ run.bat
@@ -5,14 +5,17 @@ REM # to finish - provided the target machine has all of   #
 REM # necessary software installed on their machine.       #
 REM ########################################################
 
+REM Make the Out directory for build outputs
+MKDIR .\Out
+
 REM Firstly, setup the SQL server using sqlcmd
 REM TODO: UPDATE THIS COMMAND
-SQLCMD -S SetServerName\SetInstanceName -U SetTheUsername -P SetThePassword -i .\Path\To\SetupScript.sql
+SQLCMD -S SetServerName\SetInstanceName -U SetTheUsername -P SetThePassword -i .\Database\SetupScript.sql
 
 REM Secondly, with the server instance running, startup the backend application.
 REM TODO: UPDATE THIS COMMAND
-DOTNET PUBLISH .\Path\To\App\Project.csproj -c Release -r win-x64 -o ..\backend
-START ..\backend\backend.exe
+DOTNET PUBLISH .\Backend\Project.csproj -c Release -r win-x64 -o .\Out\Backend
+START .\Out\Backend\Backend.exe
 
 REM Lastly, setup the frontend service.
 REM TODO: SET UP FRONTEND BUILD.
