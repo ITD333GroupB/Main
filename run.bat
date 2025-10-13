f2ec73bbd016282897e8c6a448f562fc5f54cdb0
Chris DeBon <97779307+realChrisDeBon@users.noreply.github.com>
2025-10-13 15:31:00 -0500

    update run.bat for SQL Server setup
    Updated SQLCMD commands for user creation and database initialization. Added registry modification for SQL Server authentication mode.

--- run.bat
+++ run.bat
@@ -9,13 +9,27 @@ REM Make the Out directory for build outputs
 MKDIR .\Out
 
 REM Firstly, setup the SQL server using sqlcmd
-REM TODO: UPDATE THIS COMMAND
-SQLCMD -S SetServerName\SetInstanceName -U SetTheUsername -P SetThePassword -i .\Database\SetupScript.sql
+
+REM Make sure SQL Server allows for basic SQL Authentication as opposed to Windows Authentication only
+REM This will -not- work if CMD isn't startup up in Administrative mode and will output a permissions error
+
+REG ADD "HKLM\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQLServer" ^
+    /v LoginMode /t REG_DWORD /d 2 /f
+
+REM Create a default user credential that we can use via the application's configuration
+SQLCMD -S localhost -E -Q "IF NOT EXISTS (SELECT 1 FROM sys.sql_logins WHERE name = 'User1') EXEC('CREATE LOGIN [User1] WITH PASSWORD = ''Pass1!'';'); ALTER LOGIN [User1] ENABLE; ALTER LOGIN [User1] WITH PASSWORD = 'Pass1!' UNLOCK;"
+
+REM Init database
+SQLCMD -S localhost -E -i "%~dp0Database\preliminary_start.sql"
+
+REM Assign default user access to database
+SQLCMD -S localhost -d TaskHubDb -E -Q "IF NOT EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'User1') BEGIN CREATE USER [User1] FOR LOGIN [User1]; ALTER ROLE db_owner ADD MEMBER [User1]; END"
+
+REM Quickly restart SQL server
+NET STOP MSSQLSERVER
+NET START MSSQLSERVER
 
 REM Secondly, with the server instance running, startup the backend application.
 REM TODO: UPDATE THIS COMMAND
 DOTNET PUBLISH .\Backend\Project.csproj -c Release -r win-x64 -o .\Out\Backend
 START .\Out\Backend\Backend.exe
-
-REM Lastly, setup the frontend service.
-REM TODO: SET UP FRONTEND BUILD.
